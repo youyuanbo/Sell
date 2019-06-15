cb0b7831d99007feb1837444eb802b93c85ca662
youyuanbo <ug.python.linux@gmail.com>
2019-06-15 09:38:47 +0800

    ..

--- sell/src/test/java/com/imooc/sell/SellApplicationTests.java
+++ sell/src/test/java/com/imooc/sell/SellApplicationTests.java
@@ -5,12 +5,25 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SellApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(1);
+
+
+
+        System.out.println(list.size());
+
     }
 
 }
